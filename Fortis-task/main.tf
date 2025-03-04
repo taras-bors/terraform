c6c222255052fad57f38282c58b6c47bb3979b14
Taras Boryshkevych <taras.boryshkevych@innovapost.com>
2025-03-04 06:20:32 -0500

    Fort tf task, start

---
+++ Fortis-task/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ca-central-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
