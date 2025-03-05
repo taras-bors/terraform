468fba89d1c49d268a0a18bd2d71ec76d78e6837
Taras Boryshkevych <taras.boryshkevych@innovapost.com>
2025-03-05 08:52:44 -0500

    Fort tf task: Adding IGW and NAT.

--- Fortis-task/vpc/main.tf
+++ Fortis-task/vpc/main.tf
@@ -29,3 +29,70 @@ resource "aws_subnet" "back_end_private" {
   availability_zone       = element(var.availability_zones, count.index)
 }
 
+# Internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+
+# NAT
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.dmz_public.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+# Route tables
+# DMZ route table
+resource "aws_route_table" "dmz_public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "dmz_public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_assoc" {
+  subnet_id      = aws_subnet.dmz_public.id
+  route_table_id = aws_route_table.dmz_public_rt.id
+}
+
+# Private route table - routing traffic from private subnets through NAT
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "front_end_private_subnet_assoc" {
+  subnet_id      = aws_subnet.front_end_private.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "back_end_private_subnet_assoc" {
+  count          = length(var.back_end_private_subnet_cidrs)
+  subnet_id      = aws_subnet.back_end_private[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
